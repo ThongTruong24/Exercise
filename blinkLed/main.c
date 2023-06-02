f4dc9e2e45b0ae1653d15cdc60e32c090241483d
ThongTruong <quangthong122333@gmail.com>
2023-06-02 16:48:20 +0700

    autosar

--- blinkLed/main.c
+++ blinkLed/main.c
@@ -1,29 +1,29 @@
 #include "stm32f10x.h"
+#include "Dio.h"
 
-void config(){
+void config();
+void delay(uint32_t indexDelay);
 
+int main() {
+	config();
+	while(1) {
+	  Dio_WriteChannel(DIO_CHANNEL_PC13, STD_HIGH);
+	  delay(1000000);
+    Dio_WriteChannel(DIO_CHANNEL_PC13, STD_LOW);
+	  delay(1000000);
+	}
+}
+	
+void config() {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 	GPIO_InitTypeDef gpio;
-	gpio.GPIO_Mode = GPIO_Mode_Out_PP;
 	gpio.GPIO_Pin = GPIO_Pin_13;
-	gpio.GPIO_Speed = GPIO_Speed_50MHz;
+	gpio.GPIO_Mode = GPIO_Mode_Out_PP;
+	gpio.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_Init(GPIOC, &gpio);
-
 }
 
-void delay(){
+void delay(uint32_t indexDelay){
 	uint32_t i;
-	for(i = 0; i < 1000000; i++);
-}
-
-int main(void){
-
-	config();
-	while(1){
-		GPIO_ResetBits(GPIOC, GPIO_Pin_13);
-		delay();
-		GPIO_SetBits(GPIOC, GPIO_Pin_13);
-		delay();
-	
-	}
+	for(i = 0; i < indexDelay; i++);
 }
