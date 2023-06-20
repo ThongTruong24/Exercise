2de06149ee9cfee5cf6e89a6cca64612bed01113
ThongTruong <quangthong122333@gmail.com>
2023-06-20 21:57:14 +0700

    stm32

---
+++ Dio/Dio.c
@@ -0,0 +1,326 @@
+/*
+* File: Dio.c 
+* Author: Truong Huynh Quang Thong
+* Date: 02/01/2003
+* Description: Declare functions written according to the autosar standard
+*/
+
+#include "Dio.h"
+
+/*
+* Function: Dio_WriteChannel
+* Description: Service to set a level of a channel
+* Input:
+*   ChannelId: ID of DIO channel
+*   Level: Value to be written
+* Output:
+*   None
+*/
+void Dio_WriteChannel (Dio_ChannelType ChannelId,Dio_LevelType Level) {
+	GPIO_TypeDef* gpioPort;
+  uint16_t gpioPin;
+  switch (ChannelId) {
+    case DIO_CHANNEL_PA0:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_0;
+      break;
+    case DIO_CHANNEL_PA1:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_1;
+      break;
+    case DIO_CHANNEL_PA2:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_2;
+      break;
+    case DIO_CHANNEL_PA3:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_3;
+      break;
+    case DIO_CHANNEL_PA4:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_4;
+      break;
+    case DIO_CHANNEL_PA5:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_5;
+      break;
+    case DIO_CHANNEL_PA6:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_6;
+      break;
+    case DIO_CHANNEL_PA7:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_7;
+      break;
+    case DIO_CHANNEL_PA8:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_8;
+      break;
+    case DIO_CHANNEL_PA9:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_9;
+      break;
+    case DIO_CHANNEL_PA10:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_10;
+      break;
+    case DIO_CHANNEL_PA11:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_11;
+      break;
+    case DIO_CHANNEL_PA12:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_12;
+      break;
+    case DIO_CHANNEL_PA13:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_13;
+      break;
+    case DIO_CHANNEL_PA14:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_14;
+      break;
+    case DIO_CHANNEL_PA15:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_15;
+      break;
+    case DIO_CHANNEL_PB0:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_0;
+      break;
+    case DIO_CHANNEL_PB1:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_1;
+    break;
+    case DIO_CHANNEL_PB2:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_2;
+      break;
+    case DIO_CHANNEL_PB3:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_3;
+      break;
+    case DIO_CHANNEL_PB4:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_4;
+      break;
+    case DIO_CHANNEL_PB5:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_5;
+      break;
+    case DIO_CHANNEL_PB6:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_6;
+      break;
+    case DIO_CHANNEL_PB7:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_7;
+      break;
+    case DIO_CHANNEL_PB8:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_8;
+      break;
+    case DIO_CHANNEL_PB9:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_9;
+      break;
+    case DIO_CHANNEL_PB10:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_10;
+     break;
+    case DIO_CHANNEL_PB11:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_11;
+      break;
+    case DIO_CHANNEL_PB12:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_12;
+      break;
+    case DIO_CHANNEL_PB13:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_13;
+      break;
+    case DIO_CHANNEL_PB14:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_14;
+      break;
+    case DIO_CHANNEL_PB15:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_15;
+      break;
+    case DIO_CHANNEL_PC13:
+      gpioPort = GPIOC;
+      gpioPin  = GPIO_Pin_13;
+      break;
+    case DIO_CHANNEL_PC14:
+      gpioPort = GPIOC;
+      gpioPin  = GPIO_Pin_14;
+      break;
+    case DIO_CHANNEL_PC15:
+      gpioPort = GPIOC;
+      gpioPin  = GPIO_Pin_15;
+      break;		
+	}
+	if (Level == STD_LOW) GPIO_ResetBits(gpioPort, gpioPin);
+	else GPIO_SetBits(gpioPort, gpioPin);		
+}
+
+/*
+* Function: Dio_ReadChannel
+* Description: Returns the value of the specified DIO channel.
+* the level of the channel after flip.
+* Input:
+*   ChannelId: ID of DIO channel
+* Output:
+*   Dio_LevelType: 
+*     STD_HIGH The physical level of the corresponding Pin is STD_HIGH
+*     STD_LOW The physical level of the corresponding Pin is STD_LOW
+*/
+Dio_LevelType Dio_ReadChannel (Dio_ChannelType ChannelId) {
+	GPIO_TypeDef* gpioPort;
+	uint16_t gpioPin;
+	switch(ChannelId) {
+		case DIO_CHANNEL_PA0:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_0;
+      break;
+    case DIO_CHANNEL_PA1:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_1;
+      break;
+    case DIO_CHANNEL_PA2:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_2;
+      break;
+    case DIO_CHANNEL_PA3:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_3;
+      break;
+    case DIO_CHANNEL_PA4:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_4;
+      break;
+    case DIO_CHANNEL_PA5:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_5;
+      break;
+    case DIO_CHANNEL_PA6:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_6;
+      break;
+    case DIO_CHANNEL_PA7:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_7;
+      break;
+    case DIO_CHANNEL_PA8:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_8;
+      break;
+    case DIO_CHANNEL_PA9:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_9;
+      break;
+    case DIO_CHANNEL_PA10:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_10;
+      break;
+    case DIO_CHANNEL_PA11:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_11;
+      break;
+    case DIO_CHANNEL_PA12:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_12;
+      break;
+    case DIO_CHANNEL_PA13:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_13;
+      break;
+    case DIO_CHANNEL_PA14:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_14;
+      break;
+    case DIO_CHANNEL_PA15:
+      gpioPort = GPIOA;
+      gpioPin  = GPIO_Pin_15;
+      break;
+    case DIO_CHANNEL_PB0:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_0;
+      break;
+    case DIO_CHANNEL_PB1:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_1;
+    break;
+    case DIO_CHANNEL_PB2:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_2;
+      break;
+    case DIO_CHANNEL_PB3:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_3;
+      break;
+    case DIO_CHANNEL_PB4:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_4;
+      break;
+    case DIO_CHANNEL_PB5:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_5;
+      break;
+    case DIO_CHANNEL_PB6:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_6;
+      break;
+    case DIO_CHANNEL_PB7:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_7;
+      break;
+    case DIO_CHANNEL_PB8:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_8;
+      break;
+    case DIO_CHANNEL_PB9:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_9;
+      break;
+    case DIO_CHANNEL_PB10:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_10;
+     break;
+    case DIO_CHANNEL_PB11:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_11;
+      break;
+    case DIO_CHANNEL_PB12:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_12;
+      break;
+    case DIO_CHANNEL_PB13:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_13;
+      break;
+    case DIO_CHANNEL_PB14:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_14;
+      break;
+    case DIO_CHANNEL_PB15:
+      gpioPort = GPIOB;
+      gpioPin  = GPIO_Pin_15;
+      break;
+    case DIO_CHANNEL_PC13:
+      gpioPort = GPIOC;
+      gpioPin  = GPIO_Pin_13;
+      break;
+    case DIO_CHANNEL_PC14:
+      gpioPort = GPIOC;
+      gpioPin  = GPIO_Pin_14;
+      break;
+    case DIO_CHANNEL_PC15:
+      gpioPort = GPIOC;
+      gpioPin  = GPIO_Pin_15;
+      break;
+	}
+  if(GPIO_ReadInputDataBit(gpioPort, gpioPin) == 0) return STD_LOW;
+	else return STD_HIGH;		
+}
---
+++ Dio/Dio.h
@@ -0,0 +1,66 @@
+/*
+* File: Dio.h 
+* Author: Truong Huynh Quang Thong
+* Date: 02/01/2003
+* Description: This is the file used to declare functions written according to the autosar standard
+*/
+
+#ifndef _DIO_H
+#define _DIO_H
+
+#include "stm32f10x.h"
+
+/* Declare an enum consisting of pin numbers */
+typedef enum {
+  DIO_CHANNEL_PA0,
+	DIO_CHANNEL_PA1,
+	DIO_CHANNEL_PA2,
+	DIO_CHANNEL_PA3,
+	DIO_CHANNEL_PA4,
+	DIO_CHANNEL_PA5,
+	DIO_CHANNEL_PA6,
+	DIO_CHANNEL_PA7,
+	DIO_CHANNEL_PA8,
+	DIO_CHANNEL_PA9,
+	DIO_CHANNEL_PA10,
+	DIO_CHANNEL_PA11,
+	DIO_CHANNEL_PA12,
+	DIO_CHANNEL_PA13,
+	DIO_CHANNEL_PA14,
+	DIO_CHANNEL_PA15,
+	
+	DIO_CHANNEL_PB0,
+	DIO_CHANNEL_PB1,
+	DIO_CHANNEL_PB2,
+	DIO_CHANNEL_PB3,
+	DIO_CHANNEL_PB4,
+	DIO_CHANNEL_PB5,
+	DIO_CHANNEL_PB6,
+	DIO_CHANNEL_PB7,
+	DIO_CHANNEL_PB8,
+	DIO_CHANNEL_PB9,
+	DIO_CHANNEL_PB10,
+	DIO_CHANNEL_PB11,
+	DIO_CHANNEL_PB12,
+	DIO_CHANNEL_PB13,
+	DIO_CHANNEL_PB14,
+	DIO_CHANNEL_PB15,
+	
+	DIO_CHANNEL_PC13,
+ 	DIO_CHANNEL_PC14,
+	DIO_CHANNEL_PC15,
+} Dio_ChannelType;
+
+/* Declare an enum consisting of value status */
+typedef enum {
+  STD_LOW,
+  STD_HIGH
+} Dio_LevelType;
+
+/* Service to set a level of a channel */
+void Dio_WriteChannel(Dio_ChannelType ChannelId, Dio_LevelType Level);
+
+/* Returns the value of the specified DIO channel */
+Dio_LevelType Dio_ReadChannel (Dio_ChannelType ChannelId);
+
+#endif
